66c7bf5865a9941e44237949d20aca4e358a28b9
Zareen Wilhelm <zareen.wilhelm@q2ebanking.com>
2018-08-31 19:28:26 -0500

    Expanding dict ;

--- descriptions.py
+++ descriptions.py
@@ -1,10 +1,13 @@
 desc = {
-    "anchor": "",
+    "anchor": "Implement security policies at build ;",
     "artifactory": "",
     "bitbucket": "",
     "cassandra": "",
-    "docker": "",
+    "docker": "Allow creation of containers from image files ;",
+    "jenkins": "Builds ;",
+    "kafka": "",
+    "nginx": "web server ;",
     "vagrant": "",
     "vericode": "",
-    "xray": "",
+    "xray": "Artifactory image scanning ;",
 }
